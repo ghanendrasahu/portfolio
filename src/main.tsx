16719fcb645ab4275a2755b39acce3a31dbccca3
ghanendrasahu <ghanendrasahu2911@gmail.com>
2026-06-18 14:15:00 +0530

    Add design system with Tailwind CSS and dark theme

---
+++ src/main.tsx
@@ -0,0 +1,14 @@
+﻿import { StrictMode } from "react";
+import { BrowserRouter } from "react-router-dom";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+
+import "./styles/globals.css";
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <StrictMode>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </StrictMode>
+);
